a79a4d7c1d0a1f7aea52e0c423daba692b7d490c
reen967 <50892512+reen967@users.noreply.github.com>
2025-05-16 13:10:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from automation_estimator import task_automation_breakdown
+from estimate_automation import task_automation_breakdown
 
 st.title("Task-Level Automation Estimator")
 soc = st.text_input("Enter SOC Code (e.g. 53-7062.00)")
