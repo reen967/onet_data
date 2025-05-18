61846a449dae0f7b8222fb7722f39bab87b8cec8
reen967 <50892512+reen967@users.noreply.github.com>
2025-05-18 09:55:28 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from estimate_automation import task_automation_breakdown
-
-st.title("Task-Level Automation Estimator")
-soc = st.text_input("Enter SOC Code (e.g. 53-7062.00)")
-
-if soc:
-    df = task_automation_breakdown(soc)
-    st.dataframe(df)
